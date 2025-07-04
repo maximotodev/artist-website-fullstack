8e7010474d306ef5ed203cf5d175b88f602660e6
maximotodev <maximotodev@proton.me>
2025-07-03 20:12:26 -0400

    PROD: Remove temporary createsuperuser from build script

--- backend/build.sh
+++ backend/build.sh
@@ -6,10 +6,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# --- ADD THIS BLOCK TO CREATE A SUPERUSER ---
-# The DJANGO_SUPERUSER_... variables will be set in Render's Environment
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
