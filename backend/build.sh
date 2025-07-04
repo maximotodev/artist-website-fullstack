5ed5ef89acec210a7ea2c23f22d001e73f9e9762
maximotodev <maximotodev@proton.me>
2025-07-03 20:09:01 -0400

    TEMP: Add createsuperuser command to build script

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,3 @@
-# backend/build.sh
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
@@ -7,3 +6,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# --- ADD THIS BLOCK TO CREATE A SUPERUSER ---
+# The DJANGO_SUPERUSER_... variables will be set in Render's Environment
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
